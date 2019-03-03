86e997a82b9da6ee0fbb9bcec095801cfb5e5ea2
alexandra <le.onze.mai@gmail.com>
2019-03-03 21:50:48 +0300

    added query-selectors

--- js/script.js
+++ js/script.js
@@ -1,16 +1,32 @@
-var numberOne = null;
-var numberTwo = null;
+// New
+const $buttonCollection = document.querySelectorAll('.js-button');
+console.log($buttonCollection);
+
+const $buttonAdd = document.querySelector('.js-button-add');
+const $buttonAdd = document.querySelector('.js-button-subtract');
+const $buttonAdd = document.querySelector('.js-button-multiply');
+const $buttonAdd = document.querySelector('.js-button-devide');
+
+const $buttonAdd = document.querySelector('.js-button-delete');
+
+const $outputDisplay = document.querySelector('.js-output');
+console.log($outputDisplay);
 
 function getInputValue(id) {
-    return document.getElementById(id).value;
+  return document.getElementById(id).value;
 }
+
+// Old
+
+var numberOne = null;
+var numberTwo = null;
 
 function stringToNumber(string) {
     return string = +string;
 }
 
 function output(result) {
-    document.getElementById('display-result').value = result;
+    $outputDisplay.value = result;
 }
 
 function addition() {
